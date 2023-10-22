c99702a690f3e5012f6468a26cf2525ebdb57e03
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-10-22 16:18:56 +0530

    Overriding in Kotlin

--- app/src/main/java/com/example/kotlin/Practice/Overriding in Kotlin.kt
+++ app/src/main/java/com/example/kotlin/Practice/Overriding in Kotlin.kt
@@ -2,4 +2,25 @@ package com.example.kotlin.Practice
 
 fun main() {
     println("Overriding in Kotlin ")
+
+    val onePlus = onePlus()
+    onePlus.display()
+    onePlus.name3
+
+}
+
+open class Mobile {
+    open val name3: String = ""
+    val size: Int = 5
+    fun makeCall() = println("calling from mobile")
+    fun powerOff() = println("shutting down")
+    open fun display() = println("simple mobile display")
+
+}
+
+class onePlus : Mobile() {
+
+
+
+    override fun display() = println("one plus display")
 }
