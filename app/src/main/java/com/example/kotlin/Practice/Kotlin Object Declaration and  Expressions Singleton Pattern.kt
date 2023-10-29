afb5fbe88758a1c04535f84a504884943eb9efb9
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-10-29 12:42:08 +0530

    Companion Object

--- app/src/main/java/com/example/kotlin/Practice/Kotlin Object Declaration and  Expressions Singleton Pattern.kt
+++ app/src/main/java/com/example/kotlin/Practice/Kotlin Object Declaration and  Expressions Singleton Pattern.kt
@@ -1,5 +1,52 @@
 package com.example.kotlin.Practice
 
 fun main() {
-    println("Kotlin Object Declaration & Expressions | Singleton Pattern")
+    println("Kotlin Object Declaration & Expressions  Singleton Pattern")
+//
+//    println(ObjA.num)
+//    ObjB.testABC()
+
+
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementFbLike()
+    SharingWidget.incrementTwitterLike()
+    SharingWidget.incrementTwitterLike()
+    SharingWidget.incrementTwitterLike()
+    SharingWidget.display()
+}
+
+object ObjA {
+
+    val num = 10
+
+
+}
+
+object ObjB {
+
+    val p: Int = 20
+
+    fun testABC() {
+        println("I am object B")
+    }
+}
+
+
+object SharingWidget {
+
+    private var twitterLikes = 0
+    private var fbLike = 0
+
+    fun incrementTwitterLike() = twitterLikes++
+    fun incrementFbLike() = fbLike++
+    fun display() = println("fecebook -$fbLike --Twitter -$twitterLikes")
+
+
 }
