876c76e970d073ec22a4e5726943e23ce5ae81fb
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-10-29 12:49:09 +0530

    Companion Object

--- app/src/main/java/com/example/kotlin/Practice/Companion Object.kt
+++ app/src/main/java/com/example/kotlin/Practice/Companion Object.kt
@@ -2,4 +2,29 @@ package com.example.kotlin.Practice
 
 fun main() {
     println("Companion Object")
+
+    MyClass.f()
+    MyClass.AnotherObject.f2()  // Only one companion object is allowed per class
+}
+
+
+class MyClass {
+
+    companion object MyObject {
+
+
+        fun f() {
+            println("Hello i am f from Object")
+        }
+
+    }
+
+    //
+
+    object AnotherObject {
+
+        fun f2() {
+            println("Hello i am f from Another Object")
+        }
+    }
 }
