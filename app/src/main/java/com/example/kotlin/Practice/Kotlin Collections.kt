4a5b28d83c91608754dd97c74f417d3c3409d03b
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-10-31 09:37:18 +0530

    Kotlin Collections

--- app/src/main/java/com/example/kotlin/Practice/Kotlin Collections.kt
+++ app/src/main/java/com/example/kotlin/Practice/Kotlin Collections.kt
@@ -3,7 +3,27 @@ package com.example.kotlin.Practice
 fun main() {
     println("Kotlin Collections")
 
-    val nums= listOf(1,2,3)
+    val nums = mutableListOf(1, 2, 3)
     println(nums.indexOf(3))
+    println(nums.contains(1))
+    nums.add(3, 143)
+    for (i in nums) {
+        println(i)
+    }
 
+    nums.remove(1)
+
+    println("After removing element ")
+    for (i in nums) {
+        println(i)
+    }
+
+
+    // Map
+
+    val studentInfo = mutableMapOf<Int, String>()
+
+    studentInfo.put(1, "Ram")
+    studentInfo.put(2, "John")
+    studentInfo.put(1, "Katie")
 }
