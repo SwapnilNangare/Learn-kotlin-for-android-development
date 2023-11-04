04645f5cc4e9df023ce7a26ed57298ff775bd444
Swapnil Nangare <swapnilnangare143@gmail.com>
2023-11-04 09:31:57 +0530

    Lambdas Expressions

--- app/src/main/java/com/example/kotlin/Practice/Lambdas Expressions.kt
+++ app/src/main/java/com/example/kotlin/Practice/Lambdas Expressions.kt
@@ -2,4 +2,31 @@ package com.example.kotlin.Practice
 
 fun main() {
     println("Lambdas Expressions")
+
+    var fn: (a: Int, b: Int) -> Int = ::sum
+
+    val lambda1: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+    val multiLineLambda = {
+        println("Hello Lambda")
+        val a = 2 + 4
+        "hello Swapnil"
+        2
+    }
+
+    println(multiLineLambda())
+
+    val singleParamLambda = { x: Int -> x * x }
+    val lamb2: (Int) -> Int = { x -> x * x }
+
+    val sayHi = { msg: String ->
+        println("Hello $msg" + "")
+    }
+
+
+}
+
+fun sum(a: Int, b: Int): Int = a + b
+
+fun calculate(a: Int, b: Int, op: (Int, Int) -> Int): Int {
+    return op(a, b)
 }
