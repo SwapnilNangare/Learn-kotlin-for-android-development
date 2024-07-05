6d92f45d4b0b17860903daf05417971bf6558c15
Swapnil Nangare <swapnilnangare143@gmail.com>
2024-07-05 13:50:44 +0530

    Merge Sorted Array

--- app/src/main/java/com/example/kotlin/Practice/Merge Sorted Array.kt
+++ app/src/main/java/com/example/kotlin/Practice/Merge Sorted Array.kt
@@ -2,33 +2,48 @@ package com.example.kotlin.Practice
 
 fun main() {
     println("Merge Sorted Array")
-    var num1 = arrayOf(1, 2, 3, 0, 0, 0)
-    var num2 = arrayOf(2, 5, 6)
+
+    var nums1 = arrayOf(1, 2, 3, 0, 0, 0)
+    var nums2 = arrayOf(2, 5, 6)
     var m = 3
     var n = 3
 
-    var ans = IntArray(m + n)
+    /*  var ans = IntArray(m + n)
 
-    for (i in 0 until m) {
-        ans[i] = num1[i]
+      for (i in 0 until m) {
+          ans[i] = num1[i]
 
+      }
+
+      for (j in 0 until n) {
+
+          ans[m + j] = num2[j]
+
+      }
+
+      ans.sort()
+
+      for (i in ans.indices) {
+          num1[i] = ans[i]
+      }
+
+      for (ansArray in ans) {
+          println("ansArray $ansArray")
+      }
+
+     */
+
+    // second solution
+
+    var index = m
+    nums2.forEach {
+        nums1[index++] = it
     }
+    nums1.sort()
 
-    for (j in 0 until n) {
-
-        ans[m + j] = num2[j]
-
+    for(i in nums1){
+        println(i)
     }
-
-    ans.sort()
-
-    for (i in ans.indices) {
-        num1[i] = ans[i]
-    }
-
-    for (ansArray in ans) {
-        println("ansArray $ansArray")
-    }
-
-
 }
+
+
